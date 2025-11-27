46c3a0696a8e5d6c5ad6afaf5962940d34a142af
Julio <julio17cs@gmail.com>
2025-11-27 17:44:58 -0300

    Adicionando templates e registration

--- projetoDevLab/projetoDevLab/urls.py
+++ projetoDevLab/projetoDevLab/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import path, include
+from .views import home
+
 
 urlpatterns = [
+    path('', home, name='home'),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+
 ]
